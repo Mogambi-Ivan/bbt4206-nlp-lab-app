7ecb7b7976da7160fb3e57f6a27421da470d3a28
Ivan M <Ivan.mogambi@strathmore.edu>
2025-11-20 20:29:08 +0300

    changes
    Co-Authored-By: BenjaminMbuthi <131853497+BenjaminMbuthi@users.noreply.github.com>
    Co-Authored-By: HezronG <130735083+HezronG@users.noreply.github.com>
    Co-Authored-By: Nerion-Okoth <131865900+Nerion-Okoth@users.noreply.github.com>
    Co-Authored-By: Shalom052 <127388897+Shalom052@users.noreply.github.com>

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ nltk.download("punkt")
 
 # Load models and vectorizers
 
-topic_model = joblib.load("model/topic_model_lda.pkl")
+topic_model = joblib.load("model/topic_model.pkl")
 topic_vectorizer = joblib.load("model/topic_vectorizer.pkl")
-sentiment_model = joblib.load("model/sentiment_classifier_binary_balanced.pkl")
-sentiment_vectorizer = joblib.load("model/topic_vectorizer_binary_balanced.pkl")
+sentiment_model = joblib.load("model/sentiment_model.pkl")
+sentiment_vectorizer = joblib.load("model/sentiment_vectorizer.pkl")
 
 stop_words = set(stopwords.words("english"))
 stemmer = PorterStemmer()
